dba8bde0395ee84f27db2a5b9f8d28e0f9c5876f
lihao <lihao960603@outlook.com>
2025-08-27 10:17:32 +0800

    更新该文件

--- test_01/29高阶函数.py
+++ test_01/29高阶函数.py
@@ -6,3 +6,15 @@ def test(a,b,f):  #这里的f是函数，我们可以传对于函数名来实现
 
 print(test(1,-1,abs))  #这里必须是不带（）的因为在函数里面有括号了，写了报错
 print(test(2,3,lambda n:n**2))
+
+
+def test2(*args):
+    def test3():
+        sum=0
+        for n in args:
+            sum+=n
+        return sum
+    return test3     #不要加括号，返回一个函数而不是函数值
+
+print(test2(1,2,3))  #这个只是返回一个函数，一个什么都没有的test3，要想有值还得用调函数的（）
+print(test2(1,2,3)())
