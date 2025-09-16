81bb92c66a45b10f9c8c3112d9c7f3e65697205f
lihao <lihao960603@outlook.com>
2025-09-16 16:53:29 +0800

    表格功能小工具上传

--- Development_Project/01excel表格切分功能.py
+++ Development_Project/01excel表格切分功能.py
@@ -3,6 +3,6 @@ import pandas as pd
 df=pd.read_excel(r'C:\Users\lihao\Desktop\dify\产品月度销量数据.xlsx')
 df_dup=df['产品'].drop_duplicates()
 for i in df_dup:
-    df[df['产品'] == i].to_excel(fr'C:\Users\lihao\Desktop\dify\产品月度销量数据_{i}.xlsx',index=False)
+    df[df['产品'] == i].to_excel(fr'C:\Users\lihao\Desktop\dify\产品月度销量数据_{i}.xlsx',index=False)  #index忽略索引列
 
 print('完成')
---
+++ Development_Project/02合并excel表格.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+
+df_001 = pd.read_excel(r'C:\Users\lihao\Desktop\dify\产品月度销量数据_产品A.xlsx')
+df_002 = pd.read_excel(r'C:\Users\lihao\Desktop\dify\产品月度销量数据_产品B.xlsx')
+df = pd.concat([df_001, df_002], ignore_index=True)  # 这个时concat的忽略索引列
+# print (df)
+# # os.listdir()   #所有的文件名列出来
+
+
+# 读取所有的excel文件
+df_list = []  # 建一个list放入元素
+path = r'C:\Users\lihao\Desktop\dify'
+
+for i in os.listdir(path):
+    if i.split('.')[1] == 'xlsx':
+        full_path = os.path.join(path, i)
+        # 读文件必须完整路径，所以要把文件路径和读到的文件拼接起来
+        df = pd.read_excel(full_path)
+        df_list.append(df)
+# 数据写入
+df_all = pd.concat(df_list, ignore_index=True)
+print(df_all)
+
+# 拼接起来路径，用join指定好路径，别的方法不行
+out_path = os.path.join(path, '表格大汇总.xlsx')
+df_all.to_excel(out_path, index=False)
+print('success')
